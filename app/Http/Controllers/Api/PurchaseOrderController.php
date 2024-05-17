b41200485b990a96253182ca7c3dd5c8585a78c4
adibhauzan <adibhauzan48@gmail.com>
2024-05-17 19:39:15 +0700

    updated Purchase Order Controller

--- app/Http/Controllers/Api/PurchaseOrderController.php
+++ app/Http/Controllers/Api/PurchaseOrderController.php
@@ -2,39 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Token;
-use App\Models\Warehouse;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\PurchaseOrder;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\ValidationTokenUpdate;
 use Illuminate\Support\Facades\Auth;
+use App\Models\ValidationTokenUpdate;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
-use App\Repositories\Token\TokenRepositoryInterface;
-use App\Service\PurchaseOrder\EloquentPurchaseOrderService;
-use App\Repositories\Inventory\InventoryRepositoryInterface;
 use App\Repositories\PurchaseOrder\PurchaseOrderRepositoryInterface;
 
-/**
- * @OA\Tag(
- *     name="PurchaseOrder",
- *     description="Endpoints for Purchase Order"
- * )
- */
-class PurchaseController extends Controller
+class PurchaseOrderController extends Controller
 {
     private $purchaseOrderRepository;
-    private $purchaseOrderService;
 
 
     /**
      * Create a new purchaseOrderController instance.
      *
      * @param PurchaseOrderRepositoryInterface $purchaseOrderRepository
-     * @param EloquentPurchaseOrderService $purchaseOrderService
      * @return void
      */
     public function __construct(PurchaseOrderRepositoryInterface $purchaseOrderRepository)
@@ -54,14 +40,14 @@ class PurchaseController extends Controller
                 'grade' => 'required|string',
                 'sku' => 'required|string|unique:purchase_orders',
                 'description' => 'required|string',
+                'attachment_image' => 'required',
                 'ketebalan' => 'required|string',
                 'setting' => 'required|string',
                 'gramasi' => 'required|string',
+                'price' => 'required|numeric',
                 'stock_roll' => 'required|numeric',
                 'stock_kg' => 'required|numeric',
                 'stock_rib' => 'required|numeric',
-                'attachment_image' => 'required',
-                'price' => 'required|numeric',
             ]);
 
 
@@ -83,11 +69,11 @@ class PurchaseController extends Controller
 
 
             $originalImageName = $request->file('attachment_image')->getClientOriginalName();
-            $extension = $request->file('attachment_image')->getClientOriginalExtension();
-            $randomFileName = Str::random(40) . '.' . $extension;
+            // $extension = $request->file('attachment_image')->getClientOriginalExtension();
+            // $randomFileName = Str::random(40) . '.' . $extension;
 
-            $request->file('attachment_image')->storeAs('public/images/PurchaseOrder', $randomFileName);
-            $request->file('attachment_image')->storeAs('public/images/InventoryTransferIn', $randomFileName);
+            $request->file('attachment_image')->storeAs('public/images/PurchaseOrder', $originalImageName);
+            $request->file('attachment_image')->storeAs('public/images/InventoryTransferIn', $originalImageName);
 
             $purchaseOrderData = [
                 'contact_id' => $request->input('contact_id'),
@@ -99,22 +85,22 @@ class PurchaseController extends Controller
                 'grade' => $request->input('grade'),
                 'sku' => $request->input('sku'),
                 'description' => $request->input('description'),
+                'attachment_image' => $originalImageName,
                 'ketebalan' => $request->input('ketebalan'),
                 'setting' => $request->input('setting'),
                 'gramasi' => $request->input('gramasi'),
+                'price' => $request->input('price'),
                 'stock_roll' => $request->input('stock_roll'),
                 'stock_kg' => $request->input('stock_kg'),
                 'stock_rib' => $request->input('stock_rib'),
-                'attachment_image' => $originalImageName,
-                'price' => $request->input('price'),
             ];
 
             $purchaseOrder = $this->purchaseOrderRepository->create($purchaseOrderData);
 
             return response()->json(['Message' => 'success create new PurchaseOrder', 'data' => $purchaseOrder], 201);
         } catch (\Exception $e) {
-            if (isset($randomFileName)) {
-                Storage::delete('public/images/PurchaseOrder/' . $randomFileName);
+            if (isset($originalImageName)) {
+                Storage::delete('public/images/PurchaseOrder/' . $originalImageName);
             }
             return response()->json(['error' => 'Failed to create PurchaseOrder. ' . $e->getMessage()], 422);
         }
@@ -243,9 +229,6 @@ class PurchaseController extends Controller
             return response()->json(['error' => 'Failed to update the NewsArticle. ' . $e->getMessage()], 500);
         }
     }
-
-
-
 
     public function delete(string $poId)
     {
